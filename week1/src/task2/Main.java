4b218c479dcad567c92027ded41cd47c93a0e163
TheGoodie <kaka.kalash@mail.ru>
2021-10-13 00:43:07 +0300

    Сделано второе задание

--- week1/src/task2/Main.java
+++ week1/src/task2/Main.java
@@ -1,4 +1,16 @@
 package task2;
 
 public class Main {
+    public static void printbytes(double bytes){
+        int i = 0;
+        String[] formats = {"B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"};
+        while ((int)(bytes/1024) > 0){
+            bytes /= 1024;
+            i++;
+        }
+        System.out.println(String.format("%.1f", bytes) + " " + formats[i]);
+    }
+    public static void main(String[] args) {
+        printbytes(Double.parseDouble(args[0]));
+    }
 }
