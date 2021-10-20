c2b1608243619eea2e9e9871a49f987d5aa35ff1
TheGoodie <kaka.kalash@mail.ru>
2021-10-21 01:40:10 +0300

    немного изменил функционал Annotation

--- week2/src/task4/annotations/Annotation.java
+++ week2/src/task4/annotations/Annotation.java
@@ -1,13 +1,19 @@
 package task4.annotations;
 
+//Пусть класс аннотации подразумевает просто подпись на каких-то координатах
 public class Annotation {
     protected String label;
+    private int x,y;
     public Annotation(String label){
+        this.label = label;
+    }
+    public Annotation(String label, int x, int y){
+        this.x = x;
+        this.y = y;
         this.label = label;
     }
     @Override
     public String toString() {
-        return "Annotation " +
-                "{" + "label = " + label  + '}';
+        return "A " + "(" + x +", " + y + "):" + label;
     }
 }
